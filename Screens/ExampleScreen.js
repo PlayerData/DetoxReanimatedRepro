35d7f5c8ec16f93641d4bb75cba1e6d7d17d5d6b
Hayden Ball <hayden@playerdata.com>
2022-03-18 11:54:33 +0000

    Simple app with animation

--- Screens/ExampleScreen.js
+++ Screens/ExampleScreen.js
@@ -1,15 +1,108 @@
-import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
+import {
+  TouchableWithoutFeedback,
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  SafeAreaView,
+} from 'react-native';
 
-export default function ExampleScreen() {
-  return (
-    <View style={{flex: 1, marginTop: 80, marginHorizontal: 20}}>
-      <Text style={{textAlign: 'center', fontSize: 30, marginBottom: 10}}>
-        Example Screen
-      </Text>
-      <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, marginHorizontal: 20, padding: 5, borderRadius: 10, }}
-        testID='ExampleTextInputId'
-      />
-    </View>
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+const windowwidth = Dimensions.get('window').width;
+const windowheight = Dimensions.get('window').height;
+const styles = StyleSheet.create({
+  root: {
+    marginTop: '40%',
+  },
+
+  addMenu: {
+    height: 68,
+    width: 68,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 40,
+    zIndex: 2,
+  },
+
+  iconContainer: {
+    padding: 14,
+    borderRadius: 68,
+    elevation: 5,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowOffset: {height: 1, width: 1},
+  },
+
+  backgroundBlur: {
+    height: windowheight * 2,
+    width: windowwidth * 2,
+    position: 'absolute',
+    top: -windowheight,
+    left: -windowwidth,
+    backgroundColor: 'black',
+    opacity: 0.6,
+  },
+
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#f00',
+  },
+});
+
+const FABButtonView = () => {
+  const [open, setOpen] = useState(false);
+  const animation = useSharedValue(0);
+  const rotation = useDerivedValue(() =>
+    interpolate(animation.value, [0, 360], [0, 360]),
   );
-}
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{rotate: `${rotation.value}deg`}],
+  }));
+
+  const toggleMenu = () => {
+    setOpen(!open);
+    if (open) {
+      // Closing Menu
+      animation.value = withTiming(0, {duration: 420});
+    } else {
+      // Opening Menu
+      animation.value = withTiming(-135, {duration: 420});
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.root}>
+      {open && (
+        <TouchableWithoutFeedback
+          accessibilityRole="image"
+          onPress={toggleMenu}>
+          <View style={styles.backgroundBlur} />
+        </TouchableWithoutFeedback>
+      )}
+      <TouchableWithoutFeedback
+        accessibilityRole="switch"
+        onPress={toggleMenu}
+        testID="SpinButton">
+        <View style={styles.addMenu}>
+          <View style={styles.iconContainer}>
+            <Animated.View style={animatedStyles}>
+              <View style={styles.box} />
+            </Animated.View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  );
+};
+
+export default FABButtonView;
--- e2e/ExampleScreen.e2e.js
+++ e2e/ExampleScreen.e2e.js
@@ -1,13 +1,13 @@
 describe('Example Screen Tests', () => {
   beforeAll(async () => {
     await device.launchApp();
+
+    await element(by.text('Example screen')).tap();
   });
 
-  it('should type into the text field', async () => {
-    await element(by.text('Example screen')).tap();
-
-    const someText = 'This is just a test (:';
-    await element(by.id('ExampleTextInputId')).typeText(someText);
-    await expect(element(by.id('ExampleTextInputId'))).toHaveText(someText);
+  it('should spin the button', async () => {
+    for (let i = 0; i < 100; i++) {
+      await element(by.id('SpinButton')).tap();
+    }
   });
 });
